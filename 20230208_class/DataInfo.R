78f808e0ffe1947fa0df17c0d8bc511a005edc4e
pangjinan <pangjinan@360.cn>
2023-02-12 00:42:49 +0800

    add Graph

--- 20230208_class/DataInfo.R
+++ 20230208_class/DataInfo.R
@@ -1,50 +1,40 @@
-
+source("./HDataMeta.R")
 source("./HGraph.R")
-#
-#datastruct
 
-#input: a<-lavaan::sem, a[which(fttable$op=='~'),c('lhs', 'rhs')]
-#output: edge of x2y
-HDataInfo <- setRefClass("HDataInfo",
-  fields = list(
-    ts              = "numeric",
-    eqs_x_set       = "vector", 
-    eqs_y_set       = "vector",
-    mid_var         = "vector",
-    x_var           = "vector",
-    y_var           = "vector",
-    lhs             = "vector",
-    rhs             = "vector",
-    est             = "vector",
-    all_var         = "vector",
-    n_var           = "numeric",
-    n_edge          = "numeric",
-    midEdgeInfo     = "matrix",
-    edgeInfo        = "matrix",
-    weighInfo       = "matrix",
-    covarInfo       = "matrix",
-    y_r2            = "numeric",
-    y_cal_variance  = "numeric",
-    var_graph_info  = "ANY",
-    mid_graph_info  = "ANY",
-    toAddDoubleWavy = "list",
-    toAddWavyStr    = "character"
-))
+HDataInfo <- setRefClass("HDataInfo", contains="HDataMeta")
 
-##input  : lavvan_class_info
-##output : which is x_var mid_var y_var
+##init function
+##input  : lavvan_class_info, origin_data
 HDataInfo$methods(
-GetVarOfProperty = function(lavvan_class_info) {
-  ts             <<- as.numeric(as.POSIXct(Sys.time()))
-  eqs_x_set      <<- lavaan::lavaanNames(lavvan_class_info,type="eqs.x")
-  eqs_y_set      <<- lavaan::lavaanNames(lavvan_class_info,type="eqs.y")
-  mid_var        <<- intersect(eqs_x_set, eqs_y_set)
-  x_var          <<- setdiff(eqs_x_set, eqs_y_set)
-  y_var          <<- setdiff(eqs_y_set, eqs_x_set)
-  all_var        <<- union(eqs_y_set, eqs_x_set)
-  n_var          <<- length(all_var)
-  var_graph_info <<- HGraphClass()
-  mid_graph_info <<- HGraphClass()
+GetVarOfProperty = function(lavvan_class_info, origin_data) {
+  ts              <<- as.numeric(as.POSIXct(Sys.time()))
+  lavvanClassInfo <<- lavvan_class_info
+  originData      <<- origin_data
+  eqs_x_set       <<- lavaan::lavaanNames(lavvan_class_info,type="eqs.x")
+  eqs_y_set       <<- lavaan::lavaanNames(lavvan_class_info,type="eqs.y")
+  mid_var         <<- intersect(eqs_x_set, eqs_y_set)
+  x_var           <<- setdiff(eqs_x_set, eqs_y_set)
+  y_var           <<- setdiff(eqs_y_set, eqs_x_set)
+  all_var         <<- union(eqs_y_set, eqs_x_set)
+  x_mid_var       <<- union(x_var, mid_var)
+  n_var           <<- length(all_var)
+  var_graph_info  <<- HGraphClass()
+  mid_graph_info  <<- HGraphClass()
+  
+  # get varDict is ov or iv
+  var2ovlvDict   <<- setNames(as.list(rep("", n_var)), all_var)
+  lv_set          <- lavaan::lavaanNames(lavvan_class_info,type="lv")
+  ov_set          <- lavaan::lavaanNames(lavvan_class_info,type="ov")
+  for (i in 1:length(ov_set)) {
+    if (ov_set[i] %in% all_var) {
+      var2ovlvDict[[ov_set[i]]] <<- "ov"
+    }
+  }
+  for (i in 1:length(lv_set)) {
+    if (lv_set[i] %in% all_var) {
+      var2ovlvDict[[lv_set[i]]] <<- "lv"
+    }
+  }
 })
 
 HDataInfo$methods(
@@ -91,6 +81,15 @@ GetCovar = function(x2yinfo_covar) {
       covarInfo[t_rhs[i], t_lhs[i]] <<- t_est[i]
     }
   } # end for
+  c_len <- length(x_mid_var)
+  x_mid_covarInfo <<- matrix(NA, c_len, c_len, dimnames = list(x_mid_var, x_mid_var))
+  for(i in 1:c_len) {
+    for(j in 1:c_len) {
+      if( !is.na(covarInfo[x_mid_var[i], x_mid_var[j]]) ){
+        x_mid_covarInfo[x_mid_var[i], x_mid_var[j]] <<- covarInfo[x_mid_var[i], x_mid_var[j]]
+      } # end if
+    } # end for-inner
+  } # end for-outer
 })
 
 HDataInfo$methods(
@@ -99,7 +98,7 @@ GetR2 = function(x2yinfo_r2) {
   t_rhs <- x2yinfo_r2['rhs'][[1]]
   t_est <- x2yinfo_r2['est'][[1]]
   c_len <- length(t_lhs)
-  # consider only one y_var
+  # !!! consider only one y_var !!!
   cur_y_var <- y_var[1]
   for (i in 1:c_len) {
     if ((t_lhs[i] == cur_y_var) && (t_rhs[i] == cur_y_var)) {
@@ -109,6 +108,11 @@ GetR2 = function(x2yinfo_r2) {
   }
   cur_y_alpha <- covarInfo[cur_y_var, cur_y_var]
   y_cal_variance <<- cur_y_alpha / (1-y_r2)
+  if (var2ovlvDict[[cur_y_var]] == 'lv') { # cur_y_var is lv
+    y_get_variance <<- lavaan::lavInspect(lavvanClassInfo,"cov.lv")[cur_y_var, cur_y_var]
+  } else { # cur_y_var is ov
+    y_get_variance <<- lavaan::lavInspect(lavvanClassInfo,"implied")$conv[cur_y_var, cur_y_var]
+  }
 })
 
 HDataInfo$methods(
@@ -136,17 +140,61 @@ AddDoubleWavy = function() {
   } # end n_mid > 1
 })
 
-GetDataInfo <- function(lavvan_class_info) {
+HDataInfo$methods(
+GetSimulateExoVar = function() {
+  cur_x_mid_covarInfo <- x_mid_covarInfo
+  cur_x_mid_covarInfo[is.na(cur_x_mid_covarInfo)] <- 0
+  # get eigen
+  cur_eigen <- eigen(cur_x_mid_covarInfo)$values
+  cur_n_sample <- length(originData[, 1])
+  cur_x_mid_mean <- rep(0, length(x_mid_var))
+  #print (cur_n_sample)
+  #check if format then mvrnorm else print error and stop
+  if(all(cur_eigen > 0)) {
+    simulateExoVar <<- MASS::mvrnorm(n = cur_n_sample,
+                         cur_x_mid_mean, cur_x_mid_covarInfo, empirical = T)
+  } else {
+    stop("x_mid_covarInfo is not a positive definite matrix")
+  }
+})
+
+HDataInfo$methods(
+GetMidCalMatrix = function() {
+  n_mid <- length(mid_var)
+  n_row <- length(originData[, 1])
+  mid_cal_matrix <<- matrix(0, n_row, n_mid, dimnames = list(rep(1:n_row), mid_var))
+  graphInfo <- var_graph_info$graphInfo
+  name2id <- var_graph_info$name2id
+
+  # cal each mid_var's M_matrix
+  for(i in 1:n_mid) {
+    cur_mid    <- mid_var[i]
+    cur_id     <- name2id[[cur_mid]]
+    cur_node   <- graphInfo[[cur_id]]
+    cur_parent <- cur_node$parent
+    if (length(cur_parent) > 0) {
+      for(j in 1:length(cur_parent)) {
+        parent_name <- cur_parent[j]
+        weight <- weighInfo[parent_name, cur_mid]
+        mid_cal_matrix[, cur_mid] <<- mid_cal_matrix[, cur_mid] + weight * simulateExoVar[, parent_name]
+      } # end for-inner
+    } # end if
+    mid_cal_matrix[, cur_mid] <<- mid_cal_matrix[, cur_mid] + simulateExoVar[, cur_mid]
+  } # end for-outer
+})
+
+GetDataInfo <- function(lavvan_class_info, origin_data) {
   ainfo         <- HDataInfo()
   #myParTable <- lavaan::parTable(lavvan_class_info)
   myParTable    <- lavaan::parameterEstimates(lavvan_class_info, rsquare = TRUE)
   x2yinfo       <- myParTable[which(myParTable$op == '~'), c('lhs', 'rhs', 'est')]
   x2yinfo_covar <- myParTable[which(myParTable$op == '~~'), c('lhs', 'rhs', 'est')]
   x2yinfo_r2    <- myParTable[which(myParTable$op == 'r2'), c('lhs', 'rhs', 'est')]
-  ainfo$GetVarOfProperty(lavvan_class_info)
+  ainfo$GetVarOfProperty(lavvan_class_info, origin_data)
   ainfo$GetMatriOfPath(x2yinfo)
   ainfo$GetCovar(x2yinfo_covar)
   ainfo$GetR2(x2yinfo_r2)
+  ainfo$GetSimulateExoVar()
   
   ainfo$var_graph_info$Runner(ainfo$edgeInfo, ainfo$all_var)
   if (length(ainfo$mid_var) > 1) {
@@ -154,5 +202,6 @@ GetDataInfo <- function(lavvan_class_info) {
   }
   
   ainfo$AddDoubleWavy()
+  ainfo$GetMidCalMatrix()
   return (ainfo)
 }
--- 20230208_class/test_unit.R
+++ 20230208_class/test_unit.R
@@ -1,5 +1,4 @@
 
-library(lavaan)
 head(PoliticalDemocracy)
 ## The industrialization and Political Democracy Example
 ## Bollen (1989), page 332
@@ -42,11 +41,30 @@ y4 ~~ y8
 y6 ~~ y8'
 fit <- lavaan::sem(model, data = PoliticalDemocracy)
 
+model <- '
+# latent variable definitions
+# ind60 =~ x1 + x2 + x3
+dem60 =~ y1 + a*y2 + b*y3 + c*y4
+dem65 =~ y5 + a*y6 + b*y7 + c*y8
+# regressions
+dem60 ~ x1
+dem65 ~ x1 + dem60
+# residual correlations
+y1 ~~ y5
+y2 ~~ y4 + y6
+y3 ~~ y7
+y4 ~~ y8
+y6 ~~ y8'
+fit <- lavaan::sem(model, data = PoliticalDemocracy)
+
 source("./DataInfo.R")
-dfm <- GetDataInfo(fit)
-a <- dfm$covarInfo
-b <- dfm
-myParTable    <- lavaan::parameterEstimates(fit, rsquare = TRUE)
+dfm <- GetDataInfo(fit, PoliticalDemocracy)
+gi <- dfm$var_graph_info$graphInfo
+model2 <- paste0(model, dfm$toAddWavyStr)
+fit2 <- lavaan::sem(model2, data = PoliticalDemocracy)
+dfm2 <- GetDataInfo(fit2, PoliticalDemocracy)
 
-myParTable <- lavaan::parameterEstimates(fit,rsquare = TRUE)
-
+b <- lavaan::parameterEstimates(fit, rsquare = TRUE)
+c <- dfm$simulateExoVar
+j <- 0.3*c[,"dem60"] + 0.2*c[, "dem61"]
+c[1, "dem60"]
